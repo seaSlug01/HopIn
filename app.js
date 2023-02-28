f60074fc34e4925df54da34c9e256fe0a8b467ee
Kwstas <deekwstaz@gmail.com>
2023-02-28 23:28:17 +0200

    asdd

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ const cloudinary = require("cloudinary").v2;
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_SECRET
+  api_secret: process.env.CLOUD_SECRET,
+  secure: true
 })
 
 const server = app.listen(process.env.PORT || devPORT, () =>
