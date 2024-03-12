4a9dc83959b4d03a8cb217e4fd0bb5b1a84cba04
unknown <deekwstaz@gmail.com>
2024-03-12 04:40:14 +0200

    Hide Creds

--- controllers/utils.js
+++ controllers/utils.js
@@ -1,118 +1,133 @@
-const path = require("path");
-const fs = require("fs");
-const cloudinary = require("cloudinary").v2;
-const {Storage} = require("@google-cloud/storage");
-const iconvlite  = require("iconv-lite");
-const srt2vtt = require('srt-to-vtt')
-
+const path = require("path")
+const fs = require("fs")
+const cloudinary = require("cloudinary").v2
+const { Storage } = require("@google-cloud/storage")
+const iconvlite = require("iconv-lite")
+const srt2vtt = require("srt-to-vtt")
 
 const gc = new Storage({
   projectId: "hopin-372300",
-  keyFilename: "hopin-372300-aada8aad0406.json"
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 })
 
-const googleCloudBucket = gc.bucket("hopin-372300_bucket");
+const googleCloudBucket = gc.bucket("hopin-372300_bucket")
 
 async function uploadToGoogleCloudBucket(filePath, goBack) {
-  let gcs = googleCloudBucket;
-  const uploadResponse = await gcs.upload(path.join(__dirname, `${goBack}${filePath}`));
-  const uploadedFilename = uploadResponse[0].metadata.name;
-  const gcFile = await gcs.file(uploadedFilename);
+  let gcs = googleCloudBucket
+  const uploadResponse = await gcs.upload(
+    path.join(__dirname, `${goBack}${filePath}`)
+  )
+  const uploadedFilename = uploadResponse[0].metadata.name
+  const gcFile = await gcs.file(uploadedFilename)
   const signedUrl = await gcFile.getSignedUrl({
-    action: 'read',
-    expires: '03-09-2491'
+    action: "read",
+    expires: "03-09-2491",
   })
-  return signedUrl[0];
+  return signedUrl[0]
 }
 
 function directoryPath(filePath, goBack) {
-  return path.join(__dirname, `${goBack}${filePath}`);
+  return path.join(__dirname, `${goBack}${filePath}`)
 }
 
 const fileUpload = async (filePath, goBack = "", options) => {
   try {
-    const response = await cloudinary.uploader.upload(directoryPath(filePath, goBack), {
-      ...options,
-      use_fileName: true,
-      folder: "HopIn",
-    });
+    const response = await cloudinary.uploader.upload(
+      directoryPath(filePath, goBack),
+      {
+        ...options,
+        use_fileName: true,
+        folder: "HopIn",
+      }
+    )
 
-    return response;
-  } catch(error) {
-    console.log(error);
+    return response
+  } catch (error) {
+    console.log(error)
   }
 }
 
 function decodeNonEnglishName(name) {
-  return iconvlite.decode(name, "utf-8");
+  return iconvlite.decode(name, "utf-8")
 }
 
 // saving files
 
 const createUserDirectory = (userId) => {
-  const currentUserDirForFiles = `/uploads/files/tmp-userFiles-${userId}`;
+  const currentUserDirForFiles = `/uploads/files/tmp-userFiles-${userId}`
 
   // create a folder dir for current users stored (temp) files if it doesn't exist
   // C:\Users\Kwstas\Desktop\projects\NodeJS\twitterClone\routes -> Current directory
-  if(!fs.existsSync(path.join(__dirname, `../${currentUserDirForFiles}`))) {
-      fs.mkdirSync(path.join(__dirname, `../${currentUserDirForFiles}`), { recursive: true });
+  if (!fs.existsSync(path.join(__dirname, `../${currentUserDirForFiles}`))) {
+    fs.mkdirSync(path.join(__dirname, `../${currentUserDirForFiles}`), {
+      recursive: true,
+    })
   }
 
-  return currentUserDirForFiles;
+  return currentUserDirForFiles
 }
 
 const getExt = (fileName) => {
-  return fileName.split(".").pop();
+  return fileName.split(".").pop()
 }
 
 const saveFile = (res, file, dir) => {
   const ext = getExt(file.originalname)
 
-  const filePath = `${dir}/${file.filename}-${file.originalname.split(".")[0]}.${ext}`
-  var tempPath = file.path;
-  var targetPath = path.join(__dirname, `../${filePath}`);
+  const filePath = `${dir}/${file.filename}-${
+    file.originalname.split(".")[0]
+  }.${ext}`
+  var tempPath = file.path
+  var targetPath = path.join(__dirname, `../${filePath}`)
 
   // replace path (uploads/files/ that comes from us declaring it in dropbox)
   // with the completed path (one that holds the custom user folder as well as the file name + extension)
-  fs.rename(tempPath, targetPath, error => {
-      if(error !== null) {
-          console.log(error)
-          return res.sendStatus((400))
-      }
+  fs.rename(tempPath, targetPath, (error) => {
+    if (error !== null) {
+      console.log(error)
+      return res.sendStatus(400)
+    }
   })
 
-  return filePath;
+  return filePath
 }
 
 function removeFile(filePath) {
-  fs.unlink(filePath, function(err) {
-    if(err && err.code == 'ENOENT') {
-        // file doens't exist
-        console.info("File doesn't exist, won't remove it.");
+  fs.unlink(filePath, function (err) {
+    if (err && err.code == "ENOENT") {
+      // file doens't exist
+      console.info("File doesn't exist, won't remove it.")
     } else if (err) {
-        // other errors, e.g. maybe we don't have enough permission
-        console.error("Error occurred while trying to remove file");
+      // other errors, e.g. maybe we don't have enough permission
+      console.error("Error occurred while trying to remove file")
     } else {
-        console.info(`removed`);
+      console.info(`removed`)
     }
-  });
+  })
 }
 
 async function removeFiles(paths, cd) {
-  const deletePromise = paths.map(path => removeFile(directoryPath(path, cd)))
-  await Promise.all(deletePromise);
+  const deletePromise = paths.map((path) => removeFile(directoryPath(path, cd)))
+  await Promise.all(deletePromise)
 }
 
 const srtToVtt = async (req, res) => {
-  const SrtFile = req.file;
+  const SrtFile = req.file
   const ext = getExt(SrtFile.originalname)
-  const isSrt = ext.toLowerCase() === "srt";
+  const isSrt = ext.toLowerCase() === "srt"
 
-  if(!isSrt) return res.sendStatus(403);
+  if (!isSrt) return res.sendStatus(403)
 
-  const convertToVTT = await fs.createReadStream(SrtFile.path)
+  const convertToVTT = await fs
+    .createReadStream(SrtFile.path)
     .pipe(srt2vtt())
-    .pipe(fs.createWriteStream(`${SrtFile.originalname.split(".").slice(0, SrtFile.originalname.split(".").length - 1)}.vtt`))
+    .pipe(
+      fs.createWriteStream(
+        `${SrtFile.originalname
+          .split(".")
+          .slice(0, SrtFile.originalname.split(".").length - 1)}.vtt`
+      )
+    )
 
   // commented out
   const dir = createUserDirectory(req.session.user._id)
@@ -124,15 +139,14 @@ const srtToVtt = async (req, res) => {
   // the srt file will go to a google cloud bucket
 
   // commented out
-  var tempPath = convertToVTT.path;
-  var targetPath = path.join(__dirname, `../${VttFilePath}`);
+  var tempPath = convertToVTT.path
+  var targetPath = path.join(__dirname, `../${VttFilePath}`)
 
-
-  fs.rename(tempPath, targetPath, error => {
-      if(error !== null) {
-          console.log(error)
-          return res.sendStatus((400))
-      }
+  fs.rename(tempPath, targetPath, (error) => {
+    if (error !== null) {
+      console.log(error)
+      return res.sendStatus(400)
+    }
   })
 
   removeFile(SrtFile.path)
@@ -140,5 +154,16 @@ const srtToVtt = async (req, res) => {
   return VttFilePath
 }
 
-
-module.exports = { directoryPath, fileUpload, googleCloudBucket, uploadToGoogleCloudBucket, decodeNonEnglishName, createUserDirectory, saveFile, getExt, srtToVtt, removeFile, removeFiles }
+module.exports = {
+  directoryPath,
+  fileUpload,
+  googleCloudBucket,
+  uploadToGoogleCloudBucket,
+  decodeNonEnglishName,
+  createUserDirectory,
+  saveFile,
+  getExt,
+  srtToVtt,
+  removeFile,
+  removeFiles,
+}
